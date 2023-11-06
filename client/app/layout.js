41e8ee9b2e3d20f374f5206ea65b1088bead58d3
saifkp517 <saifkhan501721@gmail.com>
2023-11-07 00:16:15 +0530

    removed server

---
+++ client/app/layout.js
@@ -0,0 +1,11 @@
+import '../styles/globals.css'
+
+export default function RootLayout({children}) {
+    return (
+        <html>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
