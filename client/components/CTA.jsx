13c5e6b334c84448d141ddadd1c1862674dcb12b
saifkp517 <saifkhan501721@gmail.com>
2023-11-15 19:28:38 +0530

    stable v.2.1

--- client/components/CTA.jsx
+++ client/components/CTA.jsx
@@ -1,9 +1,10 @@
+import Image from "next/image"
+
 export default function CTA() {
     return (
         <section className="my-24">
             <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-                <img className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image" />
-                <img className="w-full hidden dark:block" src="" alt="dashboard image" />
+                <Image className="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" height={100} width={100} layout='responsive' alt="dashboard image" />
                 <div className="mt-4 md:mt-0">
                     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-500 dark:text-white">Let's create more tools and ideas that brings us together.</h2>
                     <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
